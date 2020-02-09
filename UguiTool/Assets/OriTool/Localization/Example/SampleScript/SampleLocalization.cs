2f9a8af2418a7311237f97016bc2215e00618a1b
oriolus <anhlh.ictbk@gmail.com>
2020-02-09 21:01:37 +0900

    update example

--- UguiTool/Assets/OriTool/Localization/Example/SampleScript/SampleLocalization.cs
+++ UguiTool/Assets/OriTool/Localization/Example/SampleScript/SampleLocalization.cs
@@ -1,6 +1,7 @@
 ﻿using OriTool.Localization;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 namespace OriTool.Example.Localization
@@ -8,14 +9,22 @@ namespace OriTool.Example.Localization
     public class SampleLocalization : MonoBehaviour, IEventChangeLanguage
     {
         [SerializeField] private TextMeshProUGUI textMeshProUgui;
+        [SerializeField] private Button btnEn;
+        [SerializeField] private Button btnJp;
 
         private void Awake()
         {
             LocalizeManager.Register(this);
-            LocalizeManager.LanguagePack = LanguagePack.en;
+            OnClickLanguage(0);
         }
 
-        public void OnClickLanguage(int index) => LocalizeManager.LanguagePack = (LanguagePack) index;
+        public void OnClickLanguage(int index)
+        {
+            LocalizeManager.LanguagePack = (LanguagePack) index;
+            //Ui update
+            btnJp.targetGraphic.color = index == 0 ? Color.green : Color.white;
+            btnEn.targetGraphic.color = index == 1 ? Color.green : Color.white;
+        }
 
         public void OnChangeLanguageSetting() => textMeshProUgui.SetLocalizeText("HelloWorld");
 
